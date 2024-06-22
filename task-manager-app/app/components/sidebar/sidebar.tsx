7bc45d8dcd01677be297b86dd9ce33bb372a1855
Harpreet Singh <84502026+Harpreetsingh10414@users.noreply.github.com>
2024-06-22 20:03:07 +0530

    Added GlobalProvider iconsJS ContextProvider

--- task-manager-app/app/components/sidebar/sidebar.tsx
+++ task-manager-app/app/components/sidebar/sidebar.tsx
@@ -1,11 +1,57 @@
 "use client"
+import { useGlobalState } from "@/app/context/globalProvider";
 import React from "react";
 import styled from "styled-components";
+import Image from "next/image";
+import menu from "@/app/utils/menu"
+import Link from "next/link";
+import { link } from "fs";
+import { useRouter } from "next/router";
 
 function Sidebar()
 {
-    return <SidebarStyled>Sidebar</SidebarStyled>
+    const {theme} = useGlobalState();
+    console.log(theme);
+
+    const router = useRouter();
+    const handleClick = (link:string) => {
+        router.push(link);
+    }
+
+    return <SidebarStyled theme={theme} >
+        <div className="profile">
+            <div className="profile-overlay"></div>
+            <div className="image">
+                <Image width={70} height={70} src="/avatar1.png" alt="profile" />
+            </div>
+            <h1>
+                <span>Harpreet</span>
+                <span>Singh</span>
+            </h1>
+        </div>
+        <ul className="nav-iems">
+            {menu.map((item) => {
+                return <li
+                    className={"nav-item"}
+                    onClick={() => {
+                        handleClick(item.link);
+                    }}
+                >
+                    {item.icon}
+                    <Link href={item.link}>
+                        {item.title}
+                    </Link>
+                </li>
+            })}
+        </ul>
+    </SidebarStyled>
 }
 
-const SidebarStyled = styled.nav``;
+const SidebarStyled = styled.nav`
+    position: relative;
+    width: ${(props) => props.theme.sidebarWidth};
+    background-color: ${(props) => props.theme.colors.Bg2};
+    border-right: 2px solid ${(props) => props.theme.borderColor2};
+    border-radius: 1rem;
+`;
 export default Sidebar;
--- task-manager-app/app/layout.tsx
+++ task-manager-app/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "./components/sidebar/sidebar";
 import GlobalStyleProvider from "./providers/GlobalStyleProvider";
+import ContextProvider from "./providers/ContextProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
       <body className={inter.className}>
-        <GlobalStyleProvider>
-          <Sidebar />
-          {children}
-        </GlobalStyleProvider>
+        <ContextProvider>
+          <GlobalStyleProvider>
+            <Sidebar />
+            {children}
+          </GlobalStyleProvider>
+        </ContextProvider>
       </body>
     </html>
   );
